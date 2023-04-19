0c496e263a1d4cf8cce8ee8f8ab7bbd7d4337434
Hariokas <Haroldas.Zalg@gmail.com>
2023-04-19 22:45:02 +0300

    Minor refactoring

--- WPF TestPlayground/DebugWindow.xaml.cs
+++ WPF TestPlayground/DebugWindow.xaml.cs
@@ -1,95 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using Windows.Media.Control;
 using WindowsMediaController;
 using static WindowsMediaController.MediaManager;
 
-namespace WPF_TestPlayground
+namespace WPF_TestPlayground;
+
+/// <summary>
+///     Interaction logic for DebugWindow.xaml
+/// </summary>
+public partial class DebugWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for DebugWindow.xaml
-    /// </summary>
-    public partial class DebugWindow : Window
+    private static readonly MediaManager mediaManager = new();
+    private static MediaSession? currentSession = null;
+
+    public DebugWindow()
     {
-        private static readonly MediaManager mediaManager = new MediaManager();
-        private static MediaSession? currentSession = null;
+        DataContext = this;
+        InitializeComponent();
 
-        public List<MediaSession> mediaSessionList { get; set; } = new List<MediaSession>();
+        mediaManager.OnAnySessionOpened += MediaManager_OnAnySessionOpened;
+        mediaManager.OnAnySessionClosed += MediaManager_OnAnySessionClosed;
+        mediaManager.OnFocusedSessionChanged += MediaManager_OnFocusedSessionChanged;
 
-        public DebugWindow()
+        mediaManager.Start();
+    }
+
+    public List<MediaSession> mediaSessionList { get; set; } = new();
+
+    private void MediaManager_OnFocusedSessionChanged(MediaSession mediaSession)
+    {
+        Application.Current.Dispatcher.Invoke(() =>
         {
-            DataContext = this;
-            InitializeComponent();
+            //Change the focused session.
+        });
+    }
 
-            mediaManager.OnAnySessionOpened += MediaManager_OnAnySessionOpened;
-            mediaManager.OnAnySessionClosed += MediaManager_OnAnySessionClosed;
-            mediaManager.OnFocusedSessionChanged += MediaManager_OnFocusedSessionChanged;
-
-            mediaManager.Start();
-
-        }
-
-        private void MediaManager_OnFocusedSessionChanged(MediaSession mediaSession)
+    private void MediaManager_OnAnySessionClosed(MediaSession mediaSession)
+    {
+        Application.Current.Dispatcher.Invoke(() =>
         {
-            Application.Current.Dispatcher.Invoke(() =>
-            {
-                //Change the focused session.
-            });
-        }
+            foreach (var session in mediaSessionList)
+                if (session.Id == mediaSession.Id)
+                    mediaSessionList.Remove(session);
+        });
+    }
 
-        private void MediaManager_OnAnySessionClosed(MediaSession mediaSession)
+    private void MediaManager_OnAnySessionOpened(MediaSession mediaSession)
+    {
+        Application.Current.Dispatcher.Invoke(() =>
         {
-            Application.Current.Dispatcher.Invoke(() =>
-            {
-                foreach (var session in mediaSessionList)
-                {
-                    if (session.Id == mediaSession.Id)
-                        mediaSessionList.Remove(session);
-                }
-            });
-        }
+            var session = mediaSession;
 
-        private void MediaManager_OnAnySessionOpened(MediaSession mediaSession)
-        {
-            Application.Current.Dispatcher.Invoke(() =>
-            {
-                var session = mediaSession;
+            mediaSessionList.Add(session);
+        });
+    }
 
-                mediaSessionList.Add(session);
-            });
-        }
+    private void CurrentSession_OnPlaybackStateChanged(MediaSession mediaSession,
+        GlobalSystemMediaTransportControlsSessionPlaybackInfo? playbackInfo = null)
+    {
+        Application.Current.Dispatcher.Invoke(() => { UpdateUI(mediaSession); });
+    }
 
-        private void CurrentSession_OnPlaybackStateChanged(MediaSession mediaSession, GlobalSystemMediaTransportControlsSessionPlaybackInfo? playbackInfo = null)
-        {
-            Application.Current.Dispatcher.Invoke(() =>
-            {
-                UpdateUI(mediaSession);
-            });
-        }
+    private void CurrentSession_OnMediaPropertyChanged(MediaSession mediaSession,
+        GlobalSystemMediaTransportControlsSessionMediaProperties mediaProperties)
+    {
+        Application.Current.Dispatcher.Invoke(() => { UpdateUI(mediaSession); });
+    }
 
-        private void CurrentSession_OnMediaPropertyChanged(MediaSession mediaSession, GlobalSystemMediaTransportControlsSessionMediaProperties mediaProperties)
-        {
-            Application.Current.Dispatcher.Invoke(() =>
-            {
-                UpdateUI(mediaSession);
-            });
-        }
-
-        private void UpdateUI(MediaSession mediaSession)
-        {
-
-        }
-
+    private void UpdateUI(MediaSession mediaSession)
+    {
     }
 }
